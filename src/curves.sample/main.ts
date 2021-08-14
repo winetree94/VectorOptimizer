d12054c1eccd17b5f4f6c64b4b6a5fbbe443d761
winetree94 <winetree94@outlook.com>
2021-08-14 12:58:40 +0900

    update dev server environment

--- src/curves.sample/main.ts
+++ src/curves.sample/main.ts
@@ -1,13 +1,7 @@
 import { Drawer } from './drawer/drawer';
 
-document.body.style.margin = '0';
-
-const $svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-$svg.style.width = '100%';
-$svg.style.height = '100vh';
-
-document.body.innerHTML = '';
-document.body.append($svg);
+const $svg: SVGElement = document.getElementById(
+  'svg-root'
+) as unknown as SVGElement;
 
 const drawer = new Drawer($svg);
