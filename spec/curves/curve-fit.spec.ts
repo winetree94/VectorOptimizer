c96967cb0cf6fa7664c341938d521a5c481530bc
winetree94 <winetree94@outlook.com>
2021-08-25 22:25:21 +0900

    init ramer douglas test

--- spec/curves/curve-fit.spec.ts
+++ spec/curves/curve-fit.spec.ts
@@ -1,5 +1,5 @@
 import { CurveFit } from '../../src/curves/curve-fit';
-import { ExpectedCurveFitResult } from './curve-fit.sample';
+import { ExpectedCurveFitRamerDouglasResult, ExpectedCurveFitResult } from './curve-fit.sample';
 import { approximate } from '../../src/curves/math';
 import { Vector } from '../../src/curves/vector';
 import { ExpectedLinearizedSample } from './curve-preprocess.sample';
@@ -35,6 +35,40 @@ describe('CurveFitBase', () => {
       );
       expect(approximate(bezier.p3.y, 1)).toBe(
         approximate(ExpectedCurveFitResult[index].p3.y, 1)
+      );
+    });
+  });
+
+  it('CurveFit.Fit.RamerDouglasPecher', () => {
+    const linearized = ExpectedLinearizedSample.map((point) =>
+      Vector.from(point)
+    );
+    const results = new CurveFit(linearized).fit(8);
+    expect(results.length).toBe(ExpectedCurveFitRamerDouglasResult.length);
+    results.forEach((bezier, index) => {
+      expect(approximate(bezier.p0.x, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p0.x, 1)
+      );
+      expect(approximate(bezier.p0.y, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p0.y, 1)
+      );
+      expect(approximate(bezier.p1.x, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p1.x, 1)
+      );
+      expect(approximate(bezier.p1.y, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p1.y, 1)
+      );
+      expect(approximate(bezier.p2.x, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p2.x, 1)
+      );
+      expect(approximate(bezier.p2.y, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p2.y, 1)
+      );
+      expect(approximate(bezier.p3.x, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p3.x, 1)
+      );
+      expect(approximate(bezier.p3.y, 1)).toBe(
+        approximate(ExpectedCurveFitRamerDouglasResult[index].p3.y, 1)
       );
     });
   });
